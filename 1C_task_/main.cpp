528db88cc82f5680b827f50c739fbbd199323ec4
Alex Rusin <Rusin.AI@phystech.edu>
2021-04-13 19:08:55 +0300

    Initial Commit

---
+++ 1C_task_/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1C_task_
+//
+//  Created by Александр on 13.04.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
